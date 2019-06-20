ee6c55b414a43569b38b8bc0f4dcea9b246a4d3d
gugu <1225004287@qq.com>
2019-06-20 09:50:04 +0800

    单周实训项目

---
+++ src/main/java/com/votingdemo/dao/CandidateDao.java
@@ -0,0 +1,4 @@
+package com.votingdemo.dao;
+
+public interface CandidateDao {
+}
---
+++ src/main/java/com/votingdemo/entity/candidate.java
@@ -0,0 +1,40 @@
+package com.votingdemo.entity;
+
+public class candidate {
+    private String username;
+    private String project;
+    private String sex;
+    private String images;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getProject() {
+        return project;
+    }
+
+    public void setProject(String project) {
+        this.project = project;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getImages() {
+        return images;
+    }
+
+    public void setImages(String images) {
+        this.images = images;
+    }
+}
